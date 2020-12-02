fbe2ed115bc300564ed6bd6eaacd879af680dbf3
Allen Hsu <allen.hsu@fivegoodfriends.com.au>
2020-12-03 00:36:55 +1000

    Refactor day2 to use trait.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,62 +1,121 @@
-struct Policy {
+use std::convert::TryInto;
+
+trait Policy<'a> {
+    type FP: std::convert::TryFrom<&'a str, Error = Error>;
+    fn new(first_part: Self::FP, second_part: char) -> Self;
+
+    fn evaluate(&self, candidate: &str) -> bool;
+
+    fn valid_count(input: &'a str) -> Result<usize, Error>
+    where
+        Self: Sized,
+    {
+        let mut count = 0;
+
+        for line in input.lines() {
+            let mut iter = line.splitn(3, " ");
+            let range: Self::FP = iter.next().ok_or(Error::NoFirstPartOfPolicy)?.try_into()?;
+            let letter = iter
+                .next()
+                .ok_or(Error::NoLetterSpecified)?
+                .chars()
+                .next()
+                .ok_or(Error::NoLetterSpecified)?;
+
+            let policy = Self::new(range, letter);
+
+            if policy.evaluate(iter.next().ok_or(Error::NoTestCandidate)?) {
+                count += 1;
+            }
+        }
+        Ok(count)
+    }
+}
+
+struct RangePolicy {
     range: Range,
     letter: char,
 }
 
-impl Policy {
-    pub fn evaluate(&self, candidate: &str) -> bool {
+impl Policy<'_> for RangePolicy {
+    fn evaluate(&self, candidate: &str) -> bool {
         let letter_count = candidate.chars().fold(0, |mut count, c| {
             if c == self.letter {
                 count += 1;
             }
             count
         });
-        letter_count >= self.range.low && letter_count <= self.range.high
+        self.range.range.contains(&letter_count)
     }
-}
 
-struct Range {
-    low: i32,
-    high: i32,
-}
+    type FP = Range;
 
-impl Into<Range> for &str {
-    fn into(self) -> Range {
-        let mut digits = self
-            .split("-")
-            .map(|digits| digits.parse::<i32>().expect("couldn't convert digit"));
-        Range {
-            low: digits.next().expect("didn't have enough digits for low"),
-            high: digits.next().expect("didn't have enough digits for high"),
+    fn new(first_part: Self::FP, second_part: char) -> Self {
+        Self {
+            range: first_part,
+            letter: second_part,
         }
     }
 }
 
-fn part1(input: &str) {
-    let count = input
-        .lines()
-        .filter_map(|line| {
-            let mut iter = line.split(" ");
-            let range: Range = iter.next().expect("no range in string").into();
-            let letter = iter
-                .next()
-                .expect("no letter specified")
-                .chars()
-                .next()
-                .expect("rule char not found");
-            let policy = Policy { range, letter };
-
-            if policy.evaluate(iter.next().expect("no test string found")) {
-                Some(())
-            } else {
-                None
-            }
-        })
-        .count();
-    println!("part1: {}", count);
+struct Range {
+    range: std::ops::RangeInclusive<i32>,
 }
 
-struct Policy2 {
+impl std::convert::TryFrom<&str> for Range {
+    type Error = Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut digits = value.splitn(2, "-").map(|digits| digits.parse::<i32>());
+
+        Ok(Range {
+            range: digits
+                .next()
+                .ok_or_else(|| {
+                    Error::RangeConversionError(format!("couldn't find low digits {}", value))
+                })?
+                .map_err(|err| {
+                    Error::RangeConversionError(format!("couldn't parse digit {}", err))
+                })?
+                ..=digits
+                    .next()
+                    .ok_or_else(|| {
+                        Error::RangeConversionError(format!("couldn't find high digits {}", value))
+                    })?
+                    .map_err(|err| {
+                        Error::RangeConversionError(format!("couldn't parse digit {}", err))
+                    })?,
+        })
+    }
+}
+
+#[derive(Debug)]
+enum Error {
+    RangeConversionError(String),
+    NoFirstPartOfPolicy,
+    NoLetterSpecified,
+    NoTestCandidate,
+    Unparseable,
+    NoFirstPosition,
+    NoSecondPosition,
+}
+
+use std::fmt;
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {
+}
+
+fn part1(input: &str) -> Result<(), Error> {
+    println!("part1: {}", RangePolicy::valid_count(input)?);
+    Ok(())
+}
+
+struct PositionPolicy {
     positions: Positions,
     letter: char,
 }
@@ -66,62 +125,63 @@ struct Positions {
     second_position: usize,
 }
 
-impl Into<Positions> for &str {
-    fn into(self) -> Positions {
-        let mut positions = self
-            .split("-")
-            .map(|pos| pos.parse::<usize>().expect("could not parse into position"));
+impl std::convert::TryFrom<&str> for Positions {
+    type Error = Error;
 
-        Positions {
-            first_position: positions.next().expect("could not parse first position") - 1,
-            second_position: positions.next().expect("could not parse second position") - 1,
-        }
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut positions = value.splitn(2, "-").map(|pos| pos.parse::<usize>());
+
+        Ok(Positions {
+            first_position: positions
+                .next()
+                .ok_or(Error::NoFirstPosition)?
+                .map_err(|_| Error::Unparseable)?
+                - 1,
+            second_position: positions
+                .next()
+                .ok_or(Error::NoSecondPosition)?
+                .map_err(|_| Error::Unparseable)?
+                - 1,
+        })
     }
 }
 
-impl Policy2 {
+impl Policy<'_> for PositionPolicy {
     fn evaluate(&self, candidate: &str) -> bool {
         let first = candidate
             .chars()
             .nth(self.positions.first_position)
-            .expect("no char at first position") == self.letter;
+            .expect("no char at first position")
+            == self.letter;
 
         let second = candidate
             .chars()
             .nth(self.positions.second_position)
-            .expect("no char at second position") == self.letter;
+            .expect("no char at second position")
+            == self.letter;
 
         first ^ second
     }
+
+    type FP = Positions;
+
+    fn new(first_part: Self::FP, second_part: char) -> Self {
+        Self {
+            positions: first_part,
+            letter: second_part,
+        }
+    }
 }
 
-fn part2(input: &str) {
-    let count = input
-        .lines()
-        .filter_map(|line| {
-            let mut iter = line.split(" ");
-            let positions: Positions = iter.next().expect("no range in string").into();
-            let letter = iter
-                .next()
-                .expect("no letter specified")
-                .chars()
-                .next()
-                .expect("rule char not found");
-            let policy = Policy2 { positions, letter };
-
-            if policy.evaluate(iter.next().expect("no test string found")) {
-                Some(())
-            } else {
-                None
-            }
-        })
-        .count();
+fn part2(input: &str) -> Result<(), Error>{
+    let count = PositionPolicy::valid_count(input)?;
     println!("part2: {}", count);
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::fs::read_to_string("input.txt")?;
-    part1(&input);
-    part2(&input);
+    part1(&input)?;
+    part2(&input)?;
     Ok(())
 }
