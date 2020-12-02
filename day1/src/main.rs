3cdd2db90d30cfc1b12e9d4da6dee28c3cab6c99
Allen Hsu <allen.hsu@fivegoodfriends.com.au>
2020-12-02 15:07:04 +1000

    Refactor day1 to be quadratic complexity.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,45 +2,62 @@ use itertools::Itertools;
 
 const TOTAL: i32 = 2020;
 
-fn part2(nums: &[i32]) {
-    let result = nums
-        .iter()
-        .combinations(3)
-        .find(|items| items.iter().cloned().sum::<i32>() == TOTAL)
-        .expect("couldn't find 3 values that total 2020");
-    println!("{}", result.iter().fold(1, |memo, &&i| memo * i));
-}
+fn find_total(total: &i32, nums: &[i32]) -> Option<(i32, i32)> {
+    let mut map = std::collections::HashSet::new();
 
-fn part1(nums: &[i32]) -> std::collections::HashSet<&i32> {
-    let mut result = (None, None);
-
-    let mut set = std::collections::HashSet::new();
-    for num in nums {
-        let complement = TOTAL - num;
-        match set.get(&complement) {
-            Some(_) => {
-                result = (Some(num), Some(complement));
-            }
-            None => {
-                set.insert(num);
-            }
+    for &num in nums.iter() {
+        let complement = total - num;
+        match map.get(&complement) {
+            Some(complement_index) => return Some((num, complement)),
+            None => map.insert(num),
         };
     }
 
+    None
+}
+
+fn part2(nums: &[i32]) {
+    let duos : Vec<(i32, Vec<&i32>)> = nums
+        .iter()
+        .combinations(2)
+        .map(|nums| {
+            (nums.iter().cloned().sum(), nums)
+        }
+        )
+        .collect();
+
+    let hashset = nums.iter().collect::<std::collections::HashSet<&i32>>();
+    for (duo_total, nums) in duos {
+        let complement = TOTAL - duo_total;
+        match hashset.get(&complement) {
+            Some(third) => {
+                let duo_product = nums.iter().cloned().fold(1_i32, std::ops::Mul::mul);
+                println!("part2: {}", third.clone() * duo_product);
+                return;
+            }
+            None => {}
+        }
+    }
+}
+
+fn part1(nums: &[i32]) {
+    let result = find_total(&TOTAL, nums);
+
     match result {
-        (Some(a), Some(b)) => println!("{}", a * b),
+        Some((a, b)) => println!("part1: {}", a * b),
         _ => println!("couldn't find result"),
     }
-    set
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::fs::read_to_string("input.txt")?;
-    println!("{}", input);
     let nums: Vec<_> = input
         .split("\n")
         .filter_map(|entry| entry.parse::<i32>().ok())
         .collect();
+
+    let hashset = nums.iter().collect::<std::collections::HashSet<&i32>>();
+    assert!(hashset.len() == 200);
 
     part1(&nums);
     part2(&nums);
