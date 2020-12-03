f5c2ec2a926311693343aea31a4d2439fd2e3b9d
Allen Hsu <allen.hsu@fivegoodfriends.com.au>
2020-12-03 20:10:44 +1000

    Day3 part2.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,46 @@
+struct Map {
+    map: Vec<bool>,
+    height: usize,
+    width: usize,
+}
+
+impl Map {
+    fn count_trees_on_journey(&self, horizontal_motion: usize, down: usize) -> usize {
+        let mut tree_count = 0;
+        let mut current_y = 0;
+
+        loop {
+            current_y += down;
+            if current_y > self.height { break }
+            let index = (horizontal_motion * current_y) % self.width + current_y * self.width;
+            match self.map.get(index) {
+                Some(true) => tree_count += 1,
+                None => break,
+                _ => continue,
+            }
+        }
+        tree_count
+    }
+}
+
+fn part1(map: &Map) {
+    let tree_count = map.count_trees_on_journey(3, 1);
+    println!("part1: {}", tree_count);
+}
+
+fn part2(map: &Map) {
+    let result = [
+        map.count_trees_on_journey(1, 1),
+        map.count_trees_on_journey(3, 1),
+        map.count_trees_on_journey(5, 1),
+        map.count_trees_on_journey(7, 1),
+        map.count_trees_on_journey(1, 2),
+    ].iter().fold(1, std::ops::Mul::mul);
+
+    println!("part1: {:?}", result);
+
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::fs::read_to_string("input.txt")?;
     let width = input.lines().next().expect("coudln't find first line").chars().count();
@@ -6,23 +49,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let map : Vec<bool> = input.lines().flat_map(|line| {
         line.chars().map(|c| c == '#')
     }).collect();
+    let map = Map {
+        map,
+        width,
+        height,
+    };
 
-    let horizontal_motion = 3;
-    let mut tree_count = 0;
-    let mut current_y = 0;
-    let offset = width + horizontal_motion;
-    loop {
-        current_y += 1;
-        if current_y > height { break }
-        let index = (offset * current_y) % width + current_y * width;
-        dbg!("{}", index);
-        dbg!("{}", index % width);
-        match map.get(index) {
-            Some(true) => tree_count += 1,
-            None => break,
-            _ => continue,
-        }
-    }
-    println!("part1: {}", tree_count);
+    part1(&map);
+    part2(&map);
     Ok(())
 }
